9cd54c5ef98a6424add1fb4c922522854eb12bab
Lionel <Lionel5garcia@gmail.com>
2022-08-07 20:37:03 +0200

    feat: message seed done

---
+++ database/seeders/MessageSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MessageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('messages')->insert([
+            [
+                'message' => 'Entonces, qué escudo prefieres? El hyliano o el Reflectante?',
+                'user_id' => 1,
+                'channel_id' => 1,
+                'date' => '2022-08-01'
+            ],[
+                'message' => 'Pfff... el Hyliano de calle',
+                'user_id' => 2,
+                'channel_id' => 1,
+                'date' => '2022-08-01'
+            ],[
+                'message' => 'Por qué?',
+                'user_id' => 1,
+                'channel_id' => 1,
+                'date' => '2022-08-01'
+            ]
+        ]);    
+    }
+}
